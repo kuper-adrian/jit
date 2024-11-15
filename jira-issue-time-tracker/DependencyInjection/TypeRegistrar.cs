f0ee873b509f118580f6e5bbd1371d6204872dc5
Adrian Kuper <adrian.kuper@hotmail.de>
2024-11-15 15:21:09 +0100

    chore: Use primary ctor for TypeRegistrar
    JIT-1

--- jira-issue-time-tracker/DependencyInjection/TypeRegistrar.cs
+++ jira-issue-time-tracker/DependencyInjection/TypeRegistrar.cs
@@ -3,38 +3,28 @@ using Spectre.Console.Cli;
 
 namespace jira_issue_time_tracker.DependencyInjection
 {
-    internal class TypeRegistrar : ITypeRegistrar
+    internal class TypeRegistrar(IServiceCollection builder) : ITypeRegistrar
     {
-        private readonly IServiceCollection _builder;
-
-        public TypeRegistrar(IServiceCollection builder)
-        {
-            _builder = builder;
-        }
-
         public ITypeResolver Build()
         {
-            return new TypeResolver(_builder.BuildServiceProvider());
+            return new TypeResolver(builder.BuildServiceProvider());
         }
 
         public void Register(Type service, Type implementation)
         {
-            _builder.AddSingleton(service, implementation);
+            builder.AddSingleton(service, implementation);
         }
 
         public void RegisterInstance(Type service, object implementation)
         {
-            _builder.AddSingleton(service, implementation);
+            builder.AddSingleton(service, implementation);
         }
 
         public void RegisterLazy(Type service, Func<object> func)
         {
-            if (func is null)
-            {
-                throw new ArgumentNullException(nameof(func));
-            }
+            ArgumentNullException.ThrowIfNull(func);
 
-            _builder.AddSingleton(service, (provider) => func());
+            builder.AddSingleton(service, (provider) => func());
         }
     }
 }
